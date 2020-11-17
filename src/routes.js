4d81dd81927cd168a76db35b5dc06d7fe7851a31
brenosekiro <b88660501@gmail.com>
2020-11-17 15:38:15 -0300

    protect-routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store'
 
 Vue.use(Router)
 
@@ -10,6 +11,15 @@ const router = new Router({
             path: "/",
             name: "login",
             component: () => import(/* webpackChunkName: "Login" */'./components/Login.vue'),
+            beforeEnter(to, from, next) {
+                if(store.getters.userIsAuth === true) {
+                    next({
+                        name: "home"
+                    })
+                } else {
+                    next()
+                }
+            }
         },
         {
             path: "/register",
@@ -19,9 +29,21 @@ const router = new Router({
         {
             path: "/home",
             name: "home",
-            component: () => import(/* webpackChunkName: "Home" */'./components/Home.vue')
+            component: () => import(/* webpackChunkName: "Home" */'./components/Home.vue'),
+            meta: { requiresAuth: true }
         }
     ]
+})
+
+router.beforeEach((from, to ,next) => {
+    if(from.meta.requiresAuth) {
+        if(store.getters.userIsAuth === false) {
+            return next({ name: "login" })
+        }
+        next()
+    } else {
+        next()
+    }
 })
 
 export default router
